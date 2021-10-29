67667f89cc0915519da247f63bd785a9d18890f0
Karim <karim@aurora.dev>
2021-10-30 02:39:50 +0300

    Remove duplicate code

--- src/connectors.ts
+++ src/connectors.ts
@@ -3,68 +3,8 @@ import { Account } from 'near-api-js';
 import { ConnectorType } from './types';
 import { relayerConfig } from './config';
 
-export interface IConnector {
-    nearAccount: Account;
-    address: string;
-    submit(proof: Uint8Array, gas_limit: BN, payment_for_storage: BN);
-}
-
-class DepositConnector implements IConnector {
-    nearAccount: Account;
-    address: string;
-
-    constructor(nearAccount: Account, connectorContractAddress: string) {
-        this.address = connectorContractAddress;
-        this.nearAccount = nearAccount;
-    }
-
-    async submit(proof: Uint8Array, gas_limit: BN, payment_for_storage: BN) {
-        return this.nearAccount.functionCall({
-            contractId: this.address,
-            methodName: "deposit",
-            args: proof,
-            gas: gas_limit,
-            attachedDeposit: payment_for_storage
-        });
-    }
-}
-
-class FinaliseConnector implements IConnector {
-    nearAccount: Account;
-    address: string;
-
-    constructor(nearAccount: Account, connectorContractAddress: string) {
-        this.address = connectorContractAddress;
-        this.nearAccount = nearAccount;
-    }
-
-    async submit(proof: Uint8Array, gas_limit: BN, payment_for_storage: BN) {
-        return this.nearAccount.functionCall({
-            contractId: this.address,
-            methodName: "finalise_eth_to_near_transfer",
-            args: proof,
-            gas: gas_limit,
-            attachedDeposit: payment_for_storage
-        });
-    }
-}
-
-export function getConnector(nearAccount: Account, connectorType: ConnectorType): IConnector {
-    const connectorContractAddress = getConnectorAccount(connectorType);
-
-    switch (connectorType) {
-        case ConnectorType.ethCustodian:
-            return new DepositConnector(nearAccount, connectorContractAddress);
-        case ConnectorType.erc20Locker:
-            return new DepositConnector(nearAccount, connectorContractAddress);
-        case ConnectorType.eNear:
-            return new FinaliseConnector(nearAccount, connectorContractAddress);
-        case ConnectorType.erc271Locker:
-            return new FinaliseConnector(nearAccount, connectorContractAddress);
-        default:
-            console.log("SHOULD NEVER GET HERE! Connector not found");
-            return null;
-    }
+export function getConnector(nearAccount: Account, connectorType: ConnectorType): Connector {
+    return new Connector(nearAccount, getConnectorAccount(connectorType), connectorType);
 }
 
 export function getConnectorAccount(connectorType: ConnectorType): string {
@@ -78,7 +18,44 @@ export function getConnectorAccount(connectorType: ConnectorType): string {
         case ConnectorType.erc271Locker:
             return relayerConfig.nftTokenFactoryAccount;
         default:
-            console.log("SHOULD NEVER GET HERE! Connector account not found");
-            return null;
+            throw new Error("Connector account not found!");
     }
 }
+
+class Connector {
+    nearAccount: Account;
+    address: string;
+    connectorType: ConnectorType;
+
+    constructor(nearAccount: Account, connectorContractAddress: string, connectorType: ConnectorType) {
+        this.address = connectorContractAddress;
+        this.nearAccount = nearAccount;
+        this.connectorType = connectorType;
+    }
+
+    async submit(proof: Uint8Array, gasLimit: BN, paymentForStorage: BN) {
+        return this.nearAccount.functionCall({
+            contractId: this.address,
+            methodName: Connector.getConnectorSubmitMethod(this.connectorType),
+            args: proof,
+            gas: gasLimit,
+            attachedDeposit: paymentForStorage
+        });
+    }
+
+    static getConnectorSubmitMethod(connectorType: ConnectorType): string {
+        switch (connectorType) {
+            case ConnectorType.ethCustodian:
+                return "deposit";
+            case ConnectorType.erc20Locker:
+                return "deposit";
+            case ConnectorType.eNear:
+                return "finalise_eth_to_near_transfer";
+            case ConnectorType.erc271Locker:
+                return "finalise_eth_to_near_transfer";
+            default:
+                throw new Error("Connector submit method not found!");
+        }
+    }
+}
+
